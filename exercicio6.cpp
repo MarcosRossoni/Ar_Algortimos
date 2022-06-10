dac6055ba31cd15da7cd3891ec3b49b7540d7cd1
Marcos <marcosrossoni9@gmail.com>
2022-06-10 15:21:25 -0300

    alteracao nos exrercicios

--- exercicio6.cpp
+++ exercicio6.cpp
@@ -3,9 +3,11 @@
 int main(){
 	
 	int F;
+	float C;
+	
 	for (F = 40; F <= 60; F ++){
 		
-		float C = (5.0/9.0) * (F-32);
+		C = (5.0/9.0) * (F-32);
 		
 		printf("\n\nA temperatura %i em Fahrenheit corresponde a %0.2f em Celsius", F, C); 
 	}	
